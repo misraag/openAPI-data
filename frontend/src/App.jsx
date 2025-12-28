940da8fe0928f19424b0e68a8a9a3f4f38cab491
misraag <jaiofficial50@gmail.com>
2025-12-28 23:10:38 +0530

    removed code duplicacy, made categoryNews section and removed all related duplicate files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-import Entertainment from "./pages/Entertainment";
 import Home from "./pages/Home/Home";
-import National from "./pages/National";
-import Politics from "./pages/Politics";
-import Sports from "./pages/Sports";
 import StateNews from "./pages/StateNews";
-import Technology from "./pages/Technology";
-import WorldNews from "./pages/WorldNews";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import CategoryNews from "./pages/CategoryNews";
 
 function App() {
 
@@ -20,13 +15,8 @@ function App() {
       <Navbar setDarkMode={setDarkMode} darkMode={darkMode}/>
       <Routes>
         <Route path="/" element={<Home darkMode={darkMode}/>} />
-        <Route path="/World" element={<WorldNews darkMode={darkMode}/>} />
-        <Route path="/Politics" element={<Politics darkMode={darkMode}/>} />
-        <Route path="/Sports" element={<Sports darkMode={darkMode}/>} />
-        <Route path="/Technology" element={<Technology darkMode={darkMode}/>} />
-        <Route path="/Entertainment" element={<Entertainment darkMode={darkMode}/>} />
-        <Route path="/National" element={<National darkMode={darkMode}/>} />
-        <Route path="/state/:stateName" element={<StateNews darkMode={darkMode}/>} />
+        <Route path="/:category" element={<CategoryNews darkMode={darkMode} />} />
+        <Route path="/state/:stateName" element={<StateNews darkMode={darkMode} />} />
       </Routes>
       <Footer/>
     </div>
---
+++ frontend/src/pages/CategoryNews.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import NewsTiles from "../components/NewsTiles";
+
+function CategoryNews({ darkMode }) {
+  const { category } = useParams();
+
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get(`http://localhost:5000/news/${category}`)
+      .then((res) => {
+        setNews(res.data.articles || []);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, [category]);
+
+  return (
+    <NewsTiles
+      darkMode={darkMode}
+      loading={loading}
+      category={category}
+      news={news}
+    />
+  );
+}
+
+export default CategoryNews;
